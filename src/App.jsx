90791ad86bc65c846ce05e36c709c4af416bba53
Samuel Erotides Moreira Mendes <serotides@ad.para>
2023-05-31 16:06:57 -0300

    adicionado estrutura incial do header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import Router from './route/Router';
 
+const GlobalStyle = createGlobalStyle`
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
+}
+body {  
+  background-position: top center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: #ebebeb;
+}
+`;
 
 function App() {
 
   return (
     <>
-     
+      <GlobalStyle/>
+      <Router />
     </>
   );
 }
---
+++ src/route/Router.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PaginaInicial from '../pages/PaginaInicial/Chamada';
+import ErrorPage from '../pages/ErrorPage';
+import React from 'react';
+
+function Router() {
+  return ( 
+    <BrowserRouter>
+      <Routes>
+        <Route index element={ <PaginaInicial />} />
+        <Route path="*" element={ <ErrorPage /> } />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
