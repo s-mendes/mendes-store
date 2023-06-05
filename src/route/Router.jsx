2cfaf340ef511df44d8fcd28a4d67501c51e9eec
Samuel Erotides Moreira Mendes <serotides@ad.para>
2023-06-05 14:16:45 -0300

    arquivos página do produto

--- src/route/Router.jsx
+++ src/route/Router.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PaginaInicial from '../pages/PaginaInicial';
 // import ErrorPage from '../pages/ErrorPage';
 import React from 'react';
+import Product from '../pages/Product';
 
 function Router() {
   return ( 
     <BrowserRouter>
       <Routes>
         <Route index element={ <PaginaInicial />} />
+        <Route path="/product" element={ <Product /> } />
         {/* <Route path="*" element={ <ErrorPage /> } /> */}
       </Routes>
     </BrowserRouter>
